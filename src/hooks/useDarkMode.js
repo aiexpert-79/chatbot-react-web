32b89cdcb39340601167b9757e88f47d1e81a3eb
devguru <136834911+aiexpert-79@users.noreply.github.com>
2023-08-01 04:00:23 -0500

    Update useDarkMode.js
    ok

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -5,7 +5,7 @@ import useLocalStorage from './useLocalStorage';
  * A custom hook for handling dark mode toggle in a React component.
  * It stores the dark mode toggle state in the local storage, so that the
  * toggle state persists across page reloads.
- *
+
  * @returns {Array} An array containing the current dark mode toggle state
  * and a function to update it.
  */
